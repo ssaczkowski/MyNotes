2aab09cc70a2fc5931b742ff4db58ba38ffd31eb
Sabrina Saczkowski <sabrina.sacz@gmail.com>
2021-02-06 11:31:58 -0300

    I add alignment and guides with constraint layout

--- app/src/main/java/com/ssaczkowski/mynotes/MainActivity.java
+++ app/src/main/java/com/ssaczkowski/mynotes/MainActivity.java
@@ -3,12 +3,16 @@ package com.ssaczkowski.mynotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
